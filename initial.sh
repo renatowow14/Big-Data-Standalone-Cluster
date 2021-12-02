073959c91bac0a098005cd2997929de3af76a51c
renatowow14 <renatogomessilverio@gmail.com>
2021-12-02 16:29:28 -0300

    Fix Update

--- initial.sh
+++ initial.sh
@@ -1,33 +1,35 @@
 #!/bin/bash
 
 #[PRE-REQUISITOS: git, docker, docker-compose, curl, wget]
-apt-get update && apt-get -y install htop curl wget >> /dev/null
+apt-get update >> /dev/null && apt-get -y install htop curl wget >> /dev/null
+
+mkdir -p /data/big-data-storage
 
 #Verificando se o docker e docker-compose esta instalado no sistema!"
 
 if [[ $(which docker) && $(docker --version) ]]; then
-    clear
+    
     echo "Docker Instalado No Sistema!"
     sleep 2
   else
-  	clear
+  	
     echo "Docker Nao Instalado No Sistema!"
     sleep 2 
-    clear
+    
     echo "Instalado Docker no Sistema!"
     curl -fsSL https://get.docker.com -o get-docker.sh
     chmod +x get-docker.sh && sh get-docker.sh
 fi
 
 if [[ $(which docker-compose) && $(docker-compose --version) ]]; then
-    clear
+    
     echo "Docker-Compose Instalado No Sistema!"
     sleep 2
   else
-  	clear
+  	
     echo "Docker-Compose Nao Instalado No Sistema!"
     sleep 2 
-    clear
+    
     echo "Instalado Docker-Compose no Sistema!"
     curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
@@ -35,36 +37,23 @@ if [[ $(which docker-compose) && $(docker-compose --version) ]]; then
 fi
 
 if [[ $(which git) ]]; then
-    clear
+    
     echo "Git instalado no sistema!"
     sleep 2
   else
-  	clear
+  	
     echo "Git Nao Instalado No Sistema!"
     sleep 2 
-    clear
+    
     apt-get update && apt-get -y install git
 fi
 
-if [ -d "data/Big-Data-Standalone-Cluster" ] 
-then
-    echo "Directory /path/to/dir exists, GIT-PULL" 
-    cd data/Big-Data-Standalone-Cluster
-    git pull
-
-else
-    echo "Error: Directory /path/to/dir does not exists, GIT-CLONE"
-    cd data
-    git clone https://github.com/renatowow14/Big-Data-Standalone-Cluster.git
-fi
-
-
 if [[ $(docker network  ls | grep "hadoop") ]]; then
-    clear
+    
     echo "Rede Hadoop ja Criada!"
 
 else
-     clear
+     
      echo "Rede Hadoop Nao Criada!"
      echo "Criando!"
      docker network create \
@@ -76,68 +65,72 @@ else
 fi
 
 if [[ $(docker images | grep "hadoop-image") ]]; then
-    clear
+    
     echo "Imagem ja construida!"
+    cd data
   else
-  	clear
+  	
     echo "Imagem nao construida no Sistema!"
     cd data/Hadoop && docker build -t hadoop-image .
+    cd ..
 
 
 fi
 
 if [[ $(docker images | grep "flume-image") ]]; then
-    clear
+    
     echo "Imagem ja construida!"
   else
-  	clear
+  	
     echo "Imagem nao construida no Sistema!"
-    cd data/Flume && docker build -t flume-image .
-    
-
+    pwd
+    cd Flume/ && docker build -t flume-image .
+    cd ..
 fi
 
 if [[ $(docker images | grep "sqoop-image") ]]; then
-    clear
+    
     echo "Imagem ja construida!"
   else
-  	clear
+  	
     echo "Imagem nao construida no Sistema!"
-    cd data/Sqoop && docker build -t sqoop-image .
+    cd Sqoop/ && docker build -t sqoop-image .
+    cd ..
     
 
 fi
 
 if [[ $(docker images | grep "hive-image") ]]; then
-    clear
+    
     echo "Imagem ja construida!"
   else
-  	clear
+  	
     echo "Imagem nao construida no Sistema!"
-    cd data/Hive && docker build -t hive-image .
+    cd Hive/ && docker build -t hive-image .
+    cd ..
 
 fi
 
 if [[ $(docker images | grep "postgres:11.5") ]]; then
-    clear
+    
     echo "Imagem ja construida!"
   else
-  	clear
+  	
     echo "Imagem nao construida no Sistema!"
     docker pull postgres:11.5
 fi
 
 if [[ $(docker images | grep "portainer/portainer-ce") ]]; then
-    clear
+    
     echo "Imagem ja baixada!"
   else
-  	clear
+  	
     echo "Imagem nao baixada no Sistema!"
-    docker-compose -f data/Portainer/docker-compose.yml up -d
+    docker pull portainer/portainer-ce:latest
 fi
 
-cd data/data/Portainer
 docker-compose up -d
+docker-compose -f Portainer/docker-compose.yml up -d
 
 
 
